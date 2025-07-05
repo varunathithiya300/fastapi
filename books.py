def82e0ea802e91e5a528255c8a0c29b43b29b6f
Varun Athithiya <shenbagarajvarun@gmail.com>
2025-07-05 17:25:49 +0530

    sync up

--- books.py
+++ books.py
@@ -2,13 +2,21 @@ from fastapi import FastAPI, HTTPException
 from pydantic import basemodel, Field
 
 varun = FastAPI()
+
+class Book(BaseModel):
+    id: UUID
+    title: str = Field(min_length = 1)
+    author: str = Field(min_length = 1, max_length = 100)
+    description: str = Field(min_length = 1, max_length = 100)
+    rating: int = Field(gt=-1, lt=101)
+
 books = []
 
 @varun.get("/")
 def read_api():
     return {'Welcome': 'Varun'}
 
-@varun.put()
+@varun.put("/")
 def createBook():
     pass
 
---
+++ database.md
@@ -0,0 +1,2 @@
+1. we should create a python file for the connection configuration
+2. we should create a python file that contains the schema of the database
